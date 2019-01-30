b6d7948759ade476fcbab8eb89f03416d91d533a
yanbin <yanbin>
2019-01-30 22:42:15 +0800

    新增隨堂測驗

---
+++ code/page21.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+import random
+
+x = range(0, 120)
+y = [random.randint(20, 35) for i in range(120)]
+
+plt.figure(figsize=(20, 8), dpi=80)
+
+plt.plot(x, y)
+
+plt.show()
